f1ef365df4568ebe99d8e01f4585a2616861c1c0
olga-kri <krivenkoolga1987@gmail.com>
2024-01-21 23:21:41 +0500

    Homework - Library

--- src/main/java/pro/sky/StreamAPI/model/Employee.java
+++ src/main/java/pro/sky/StreamAPI/model/Employee.java
@@ -1,6 +1,11 @@
 package pro.sky.StreamAPI.model;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Locale;
 import java.util.Objects;
+
+import static org.apache.commons.lang3.StringUtils.*;
 
 public class Employee {
     private final String firstName;
@@ -9,8 +14,8 @@ public class Employee {
     private int salary;
 
     public Employee(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
+        this.firstName = capitalize(firstName.toLowerCase());
+        this.lastName = capitalize(lastName.toLowerCase());
         this.departmentID = departmentID;
         this.salary = salary;
     }
--- src/main/java/pro/sky/StreamAPI/service/EmployeeServiceImpl.java
+++ src/main/java/pro/sky/StreamAPI/service/EmployeeServiceImpl.java
@@ -1,14 +1,18 @@
 package pro.sky.StreamAPI.service;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 import pro.sky.StreamAPI.exception.EmployeeAlreadyAddedException;
 import pro.sky.StreamAPI.exception.EmployeeNotFoundException;
+import pro.sky.StreamAPI.exception.InvalidInputException;
 import pro.sky.StreamAPI.model.Employee;
 
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.apache.commons.lang3.StringUtils.*;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -19,6 +23,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
     @Override
     public Employee add(String firstName, String lastName) {
+        validateInput(firstName,lastName);
         Employee employee = new Employee(firstName, lastName);
         if (employees.containsKey(employee.getFullName())) {
             throw new EmployeeAlreadyAddedException();
@@ -28,7 +33,7 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
     @Override
     public Employee remove(String firstName, String lastName) {
-
+        validateInput(firstName,lastName);
             Employee employee = new Employee(firstName, lastName);
             if (employees.containsKey(employee.getFullName())) {
                 return employees.remove(employee.getFullName());
@@ -38,6 +43,7 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
     @Override
     public Employee find(String firstName, String lastName) {
+        validateInput(firstName,lastName);
         Employee employee = new Employee(firstName, lastName);
         if (employees.containsKey(employee.getFullName())) {
             return employees.get(employee.getFullName());
@@ -51,5 +57,11 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public Collection<Employee> findAll() {
         return Collections.unmodifiableCollection(employees.values());
+    }
+
+    private void validateInput (String firstName, String lastName){
+        if (!isAlpha(firstName)&& isAlpha(lastName))
+            throw new InvalidInputException();
+
     }
 }
